9db82e905b045352c286c2708a4ac4edadff56bb
K0I0L0A <XYZ@example.com>
2025-12-18 16:45:05 +0100

    update exam.js

--- exam.js
+++ exam.js
@@ -59,7 +59,7 @@ do {
 let row;
 row = "";
 row = row + "*";
-*/
+
 let i;
 let row;
 row = "";
@@ -67,3 +67,88 @@ for (i = 1; i <= 5; i++) {
   row = row + "*";
 }
 console.log(row);
+let row;
+row = "";
+row = row + "*"
+row = row + "*";
+row = row + "*";
+console.log(row);
+
+let i;
+let j;
+let row;
+for (i = 1; i <= 5; i++) {
+  row = "";
+  for (j = 1; j <= 5; j++) {
+    row = row + "*";
+  }
+  console.log(row);
+}
+  
+
+let i;
+let j;
+let row;
+let n = 5;
+for (i = 1; i <= n; i++) {
+  row = "";
+  for (j = 1; j <= n; j++) {
+    if (i == 1 || i == n || j == 1 || j == n) {
+      row = row + i;
+    } else {
+      row = row + " ";
+    }
+  }
+  console.log(row);
+}
+
+
+let i;
+let j;
+let row;
+let n = 16;
+for (i = 0; i < n; i++) {
+  row = "";
+  for (j = 0; j < n; j++) {
+    if (
+      i == 0 ||
+      i == n - 1 ||
+      j == 0 ||
+      j == n - 1 ||
+      j == Math.floor(n / 2) ||
+      i == Math.floor(n / 2) ||
+      i == j ||
+      i + j == n - 1 ||
+      i + j == Math.floor(n / 2) ||
+      i - j == Math.floor(n / 2) ||
+      i + j == n - 1 + Math.floor(n / 2) ||
+      j - i == Math.floor(n / 2)
+    ) {
+      row = row + "*";
+    } else {
+      row = row + " ";
+    }
+  }
+  console.log(row);
+}
+
+//no input no output function
+let a = 100;
+let b = 200;
+function add() {
+  let c;
+  c = a + b;
+  console.log(c);
+}
+add();
+*/
+//no input with output function
+let a = 100;
+let b = 200;
+function add() {
+  let c;
+  c = a + b;
+  return c;
+}
+let res = add();
+console.log(res);
